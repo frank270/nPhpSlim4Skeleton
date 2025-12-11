87c9cfd382e6f153bd52f94858be603393f652b8
N.wang <frank@imusm.net>
2025-12-11 17:11:55 +0800

    feat: 新增門市據點頁面英雄區塊、首頁模板與QA同步腳本，並調整主版型導覽列連結及HomeAction。

--- app/actions/HomeAction.php
+++ app/actions/HomeAction.php
@@ -19,8 +19,9 @@ class HomeAction extends BaseAction
     }
     public function landingPage(Request $request, Response $response, array $args): Response
     {
-        return $this->view->render($response, 'landing.twig', [
-            'title' => 'Coming Soon - 1fBreakfast'
+        return $this->view->render($response, 'frontend/home/index.twig', [
+            'locations' => $items,
+            'filters' => $filters,
         ]);
     }
 }
