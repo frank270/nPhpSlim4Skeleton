844ca44db96c16c57ce0623e0333ed8d4bc80162
N.wang <frank@imusm.net>
2025-12-16 14:23:15 +0800

    refactor: 統一登入頁面模板路徑的大小寫。

--- app/actions/Opanel/AuthAction.php
+++ app/actions/Opanel/AuthAction.php
@@ -12,7 +12,7 @@ class AuthAction extends BaseAction
 {
     public function showLogin(Request $request, Response $response): Response
     {
-        return $this->view->render($response, 'Opanel/login.twig');
+        return $this->view->render($response, 'opanel/login.twig');
     }
     public function login(Request $request, Response $response): Response
     {
